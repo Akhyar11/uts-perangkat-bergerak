ecd37e709431d01f527dad076b5c6b3070011fca
Akhyar <akhyarakhyar725@gmail.com>
2024-04-26 12:37:35 +0700

    api fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { apiLink } from "@/data.json";
+
 export default function Home() {
+  const getNodes = async () => {
+    const resolve = await axios.get(apiLink);
+    console.log(resolve);
+  };
+
+  useEffect(() => {
+    getNodes();
+  }, []);
   return <></>;
 }
